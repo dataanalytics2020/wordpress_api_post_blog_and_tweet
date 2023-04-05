bc55417a2a669f0bc8bc2df06aad894e8246ee82
slotdata2020 <slotdata2020@gmail.com>
2023-04-05 22:08:53 +0900

    タスクスケジューラーで日時の店舗情報をWORDPRESSのMYSQLに入れるまでの処理完了#26

--- src/toukai_schedule_scraping_to_mysql.py
+++ src/toukai_schedule_scraping_to_mysql.py
@@ -1,40 +1,34 @@
-from selenium import webdriver
-import time
 import pandas as pd
-import re
-from selenium.webdriver.common.by import By
-#from datetime import datetime, date, timedelta
-from selenium.webdriver.chrome.options import Options
+import datetime
+import time
+import unicodedata
+import string
 import requests
-import json
-
-from webdriver_manager.chrome import ChromeDriverManager
-import mysql
 import mysql.connector
 import os
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+
+from sshtunnel import SSHTunnelForwarder
+import pymysql as db
+import pandas as pd
 import datetime
-
-
+import sshtunnel
+import os
+# .env ファイルをロードして環境変数へ反映
 from dotenv import load_dotenv
-load_dotenv(".env")
-
-
-print('ライブラリの読み込み完了')
-
-def insert_data_bulk(df,cnx):
-    insert_sql = """INSERT INTO mysql_database.schedule (都道府県, イベント日, 曜日, 店舗名, 取材名, 媒体名, 取材ランク,取得時間) values (%s,%s,%s,%s,%s,%s,%s,%s)"""
-    cur = cnx.cursor()
-    cur.executemany(insert_sql, df.values.tolist())
-    print("Insert bulk data")
-
+load_dotenv('.env')
+# 環境変数を参照
 
 def removal_text(text):
     text = unicodedata.normalize("NFKC", text)
     text = text.translate(str.maketrans( '', '',string.punctuation  + '！'+ '　'+ ' '+'・'+'～' + '‐'))
     return text
-
-
 
 def post_line_text(message,token):
     url = "https://notify-api.line.me/api/notify"
@@ -51,176 +45,129 @@ def post_line_text_and_image(message,image_path,token):
     files = {"imageFile":open(image_path,'rb')}
     post = requests.post(url ,headers = headers ,params=payload,files=files) 
 
+def insert_data_bulk(df,cnx):
+    insert_sql = f"""INSERT INTO {os.getenv('WORDPRESS_DB_TABLE')}(店舗名, 日付, Nのつく日, 都道府県, 機種名, 台番号, G数, 差枚, BB, RB,ART, BB確率, RB確率, ART確率, 合成確率) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    cur = cnx.cursor()
+    cur.executemany(insert_sql, df.values.tolist())
+    cnx.commit()
+    print("Insert bulk data")
 
-def login_scraping_site(area_name):
-    options = Options()
-    #options.add_argument('--headless')
-    options.add_argument("--no-sandbox")
+def delete_data(cnx,day):
+    cursor = cnx.cursor()
+    target_days_ago = datetime.date.today() - datetime.timedelta(days=day)
+    target_days_ago_str = target_days_ago.strftime('%Y-%m-%d')
+    target_days_ago_str
+    sql = f"DELETE FROM {os.getenv('WORDPRESS_DB_TABLE')} WHERE 日付 < '{target_days_ago_str} 00:00:00';"
+    cursor.execute(sql)
+    cnx.commit()
 
-    browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)#ChromeDriverManager().install() 
-    browser.implicitly_wait(10)
-    url_login = f"https://{os.getenv('SCRAPING_SYUZAI_DOMAIN')}/login_form_mail"
-    #admageを開く
-    browser.get(url_login)
-    browser.implicitly_wait(10)
+options = Options()
+options.add_argument("--headless")
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
+# options.add_argument("--disable-gpu")
+# options.add_argument("--disable-features=NetworkService")
+# options.add_argument("--window-size=1920x1080")
+# options.add_argument("--disable-features=VizDisplayCompositor")
+# UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+# options.add_argument('--user-agent=' + UA)
+# mobile_emulation = { "deviceName": "Galaxy S5" }
+# options.add_experimental_option("mobileEmulation", mobile_emulation)
 
-    # id
-    element = browser.find_element(By.NAME,"id")
-    element.click()
-    element.clear()
-    browser.implicitly_wait(10)
-    element.send_keys(os.getenv('REPORT_SITE_ID'))
+prefecture_list = ['愛知県']#,'東京都'
 
-    # pw
-    element = browser.find_element(By.NAME,"pass")
-    element.click()
-    element.clear()
-    browser.implicitly_wait(10)
-    element.send_keys(os.getenv('REPORT_SITE_PW'))
-
-    browser.implicitly_wait(10)
-    element = browser.find_element(By.CLASS_NAME,"box_hole_view_report_input")
-    element.click()
-    browser.implicitly_wait(10)
-    url = f"https://{os.getenv('SCRAPING_SYUZAI_DOMAIN')}/select_area"
-    browser.get(url)
-    browser.implicitly_wait(10)
-    url = f"https://{os.getenv('SCRAPING_SYUZAI_DOMAIN')}/?area={area_name}"
-    browser.get(url)
-    browser.implicitly_wait(10)
-    return browser
-
-
-furture_syuzai_list_df = pd.DataFrame(index=[], columns=['都道府県','イベント日','店舗名','取材名','媒体名','取材ランク'])
-browser = login_scraping_site('chubu')
-elements = browser.find_elements(By.CLASS_NAME,"mgn_serch_list_bottom")
-i = 0
-while True:
-    browser.find_element(By.CLASS_NAME,"head_change_main").click()
-    browser.implicitly_wait(10)
-    if 'プレミアム会員登録' == browser.find_element(By.CLASS_NAME,"menu_child").text:
-        browser = login_scraping_site('chubu')
-    else:
-        pass
+line_token = os.getenv('LINE_TOKEN')
+#print(line_token)
+for prefecture in prefecture_list:
     try:
-        elements = browser.find_elements(By.CLASS_NAME,"mgn_serch_list_bottom")
-        baitai_name = elements[i].text.split(' ')[0]
-        baitai_name = baitai_name.replace('パチマガスロマガじゃ…','パチマガスロマガ').replace('パチンコ店長のホール…','パチンコ店長のホール攻略')
+        post_line_text(f'{prefecture}MYSQL追加処理を開始します',line_token)
+        cols = ['機種名', '台番号', 'G数', '差枚', 'BB', 'RB', 'ART', 'BB確率', 'RB確率', 'ART確率','合成確率','店舗名']
+        ichiran_all_tennpo_df = pd.DataFrame(index=[], columns=cols)
+        yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+        browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+        url = f'https://{os.getenv("SCRAPING_DOMAIN")}/%E3%83%9B%E3%83%BC%E3%83%AB%E3%83%87%E3%83%BC%E3%82%BF/{prefecture}/'
+        browser.get(url)
+        html = browser.page_source.encode('utf-8')
+        soup = BeautifulSoup(html, 'lxml')
+        tenpo_ichiran_df = pd.read_html(html)[-1]
+        #print(tenpo_ichiran_df['ホール名'])
 
-        if  ('県' in baitai_name) or ('府' in baitai_name) or ('東京都' in baitai_name) or ('北海道' in baitai_name):
-            print('県がついているためパスを処理しました',baitai_name)
+        i = 0
+        for tenpo_name in tenpo_ichiran_df['ホール名'] :#tenpo_ichiran_df['ホール名']
+            try:
+                #print(tenpo_name)
+                url = f'https://{os.getenv("SCRAPING_DOMAIN")}/{yesterday.strftime("%Y-%m-%d")}-{tenpo_name}'
+                browser.get(url)
+                html = browser.page_source.encode('utf-8')
+                dfs = pd.read_html(html)
+                #display(tenpo_df)
+                time.sleep(1)
+                for df in  dfs:
+                #print(df.columns)
+                    if '機種名' in list(df.columns):
+                        ichiran_df = df
+                        ichiran_df['日付'] = yesterday.strftime('%Y-%m-%d')
+                        ichiran_df['店舗名'] = tenpo_name
+                        print(tenpo_name)
+                        ichiran_df['Nのつく日'] = yesterday.strftime('%d')[-1]
+                        ichiran_df['都道府県'] = prefecture 
+                        ichiran_df['機種名'] = ichiran_df['機種名'].map(removal_text)
+                        ichiran_all_tennpo_df =  pd.concat([ichiran_all_tennpo_df, ichiran_df])
+                        break
+            except:
+                time.sleep(1)
+                continue
             i += 1
-            continue
+            # if i > 0:
+            #     break
+        browser.quit()
+        cols = ichiran_all_tennpo_df.columns.tolist()
+        cols = cols[-4:] + cols[:-4]
+        ichiran_all_tennpo_df = ichiran_all_tennpo_df[cols]  #    OR    df = df.ix[:, cols]
+        ichiran_all_tennpo_df['ART']= ichiran_all_tennpo_df['ART'].fillna(0)
+        ichiran_all_tennpo_df['BB']= ichiran_all_tennpo_df['BB'].fillna(0)
+        ichiran_all_tennpo_df['RB']= ichiran_all_tennpo_df['RB'].fillna(0)
+        ichiran_all_tennpo_df['差枚']= ichiran_all_tennpo_df['差枚'].fillna(0)
+        ichiran_all_tennpo_df['G数']= ichiran_all_tennpo_df['G数'].fillna(0)
+        ichiran_all_tennpo_df = ichiran_all_tennpo_df.fillna('')
+        #print(ichiran_all_tennpo_df.iloc[:5])
+        # SSH 接続 踏み台接続
+        with sshtunnel.SSHTunnelForwarder(
+            (os.getenv('SSH_USERNAME'), 10022), 
+            ssh_username="pachislot777", 
+            ssh_private_key_password=os.getenv('SSH_PRIVATE_KEY_PASSWORD'), 
+            ssh_pkey=r"sercret\akasaka.key", 
+            remote_bind_address=("mysql8055.xserver.jp", 3306 )
+            ) as server:
 
-        print(baitai_name)
-        elements[i].click()
-        # if 'ホールナビ' in baitai_name:
-        #     print(baitai_name)
-        #     break
-        while True:
-            for syuzai_tenpo_data in browser.find_elements(By.CLASS_NAME,"osbox"):
-                tenpo_name = syuzai_tenpo_data.find_element(By.CLASS_NAME,"oslh2").text.replace('\n', '').replace(' ', '').replace('　', '')
-                #print(tenpo_name.text)
-                syuzai_date = syuzai_tenpo_data.find_element(By.CLASS_NAME,"oslmd").text
-                rank_and_syuzai_name = syuzai_tenpo_data.find_element(By.CLASS_NAME,"list_event_name").text
-                syuzai_rank = rank_and_syuzai_name.split('\n')[0]
-                syuzai_name = rank_and_syuzai_name.split('\n')[1]
-                prefectures = syuzai_tenpo_data.find_elements(By.CLASS_NAME,"oslha")[0].text
-                #print(baitai_name,syuzai_date ,syuzai_rank,syuzai_name,prefectures)#prefectures
-                record = pd.Series([prefectures,syuzai_date, tenpo_name,syuzai_name,baitai_name,syuzai_rank], index=furture_syuzai_list_df.columns)
-                furture_syuzai_list_df = furture_syuzai_list_df.append(record, ignore_index=True)
-                #print(record)
-                #break
-                browser.implicitly_wait(10)
-            if browser.find_element(By.CLASS_NAME,'navi_1_next').text == '次へ':
-                browser.find_element(By.CLASS_NAME,'navi_1_next').click()
-            else:
-                break
-        i += 1
-        # time.sleep(1)
-        #break
-        #break
+            # SSH接続確認
+            print(f"local bind port: {server.local_bind_port}")
+            # データベース接続
+            cnx = mysql.connector.connect(
+                host="localhost", 
+                port=server.local_bind_port, 
+                user=os.getenv('WORDPRESS_DB_ID'), 
+                password=os.getenv('DB_PASSWORD'), 
+                database=os.getenv('WORDPRESS_DB_NAME'), 
+                charset="utf8",
+                use_pure=True
+                )
+
+            # 接続確認
+            print(f"sql connection status: {cnx.is_connected()}")
+            cursor = cnx.cursor()
+            insert_data_bulk(ichiran_all_tennpo_df,cnx)
+            tenpo_name_number = len(ichiran_all_tennpo_df['店舗名'].unique())
+            post_line_text(f'{prefecture} {tenpo_name_number}件のSQL追加処理に成功しました',line_token)
+            delete_data(cnx,35)
+            # 終了
+            cnx.close()
+
     except Exception as e:
-        print('エラー理由',e)
-        break
-    # if i > 13:
-    #break
+        print(e)
+        post_line_text(f'{prefecture}処理失敗{e}',line_token)
 
-browser.quit()
-
-
-pattern = '東京都|北海道|(京都|大阪)府|.{2,3}県'
-# 都道府県を抽出する
-furture_syuzai_list_df = furture_syuzai_list_df[furture_syuzai_list_df['都道府県'] != '']
-# furture_syuzai_list_df['都道府県']=furture_syuzai_list_df['都道府県'].apply(lambda x:re.match(pattern,x).group())
-prefectures_list = []
-for adress in furture_syuzai_list_df['都道府県']:
-    adress = adress.split(']')[-1]
-    try:
-        #print(re.match(pattern,adress).group())
-        prefectures_list.append(re.match(pattern,adress).group())
-    except:
-        prefectures_list.append('情報なし')
-furture_syuzai_list_df['都道府県'] = prefectures_list
-furture_syuzai_list_df_1 = furture_syuzai_list_df
-furture_syuzai_list_df_1 = pd.concat([furture_syuzai_list_df_1, furture_syuzai_list_df_1['イベント日'].str.split('(', expand=True)], axis=1).drop('イベント日', axis=1)
-furture_syuzai_list_df_1.rename(columns={0: 'イベント日', 1: '曜日'}, inplace=True)
-furture_syuzai_list_df_1['曜日'] = furture_syuzai_list_df_1['曜日'].map(lambda x:x.replace(')',''))
-furture_syuzai_list_df_1['イベント日'] = pd.to_datetime(furture_syuzai_list_df_1['イベント日'])
-furture_syuzai_list_df_1 = furture_syuzai_list_df_1 [['都道府県','イベント日','曜日',	'店舗名','取材名','媒体名','取材ランク']]
-furture_syuzai_list_df_1
-
-
-# 現在時刻
-today = datetime.datetime.now()
-print(today)
-#[結果] 2021-08-23 07:12:20.806648
-# 1日後
-today_str:str = today.strftime('%Y-%m-%d')
-eight_days_after:str = (today + datetime.timedelta(days=8)).strftime('%Y-%m-%d')
-yesterday:str = (today + datetime.timedelta(days=-2)).strftime('%Y-%m-%d')
-#### Create dataframe from resultant table ####
-
-cnx = mysql.connector.connect(
-                        user = os.getenv('DB_USER_NAME'),
-                        password=os.getenv('DB_PASSWORD'), 
-                        host=os.getenv('DB_HOST'), 
-                        port='3306',
-                        database=os.getenv('DB_NAME'))
-
-cursor = cnx.cursor()
-
-
-prefectures ="("
-prefecture_list = ['愛知県','静岡県','山梨県','長野県','岐阜県','新潟県','富山県','石川県','福井県','三重県']
-for i,text in enumerate(prefecture_list):
-    if i == (len(prefecture_list) -1 ):
-        prefectures += f"'{text}'"
-    else:
-        prefectures += f"'{text}',"
-prefectures += ')'
-print(prefectures)
-
-
-sql = f"""
-        SELECT *
-        FROM schedule
-        WHERE イベント日 BETWEEN '{yesterday}' AND '{eight_days_after}'
-        AND  都道府県 in {prefectures}
-        """
-print(sql)
-cursor.execute(sql)
-#cols = [col[0] for col in cursor.description]
-sql_syuzai_report_all_df = pd.DataFrame(cursor.fetchall(),columns = ['id','都道府県','イベント日','曜日','店舗名','取材名','媒体名','取材ランク','取得時間'])
-sql_syuzai_report_all_df.sort_values('イベント日')
-sql_syuzai_report_all_df = sql_syuzai_report_all_df[['都道府県','イベント日','曜日','店舗名','取材名','媒体名','取材ランク',]]
-
-concat_df = pd.concat([sql_syuzai_report_all_df,furture_syuzai_list_df_1])
-concat_df['イベント日'] = pd.to_datetime(concat_df['イベント日'])
-concat_df = concat_df[~concat_df.duplicated(keep=False)]
-concat_df['取得時間'] = datetime.datetime.now()
-
-insert_data_bulk(concat_df ,cnx)
-cnx.commit()
-
-post_line_text(f'{len(concat_df )}件の東海の取材予定追加おわり',os.getenv('LINE_TOKEN'))
+    finally:
+        print('終了')
