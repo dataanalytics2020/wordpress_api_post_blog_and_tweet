19c373f7f71a8e5f427abfcb8c4e18ede9144b36
slotdata2020 <slotdata2020@gmail.com>
2023-04-05 23:58:58 +0900

    東海4県の結果を取得できるように変更#30

--- src/toukai_schedule_scraping_to_mysql.py
+++ src/toukai_schedule_scraping_to_mysql.py
@@ -61,22 +61,7 @@ def delete_data(cnx,day):
     cursor.execute(sql)
     cnx.commit()
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
-# options.add_argument("--disable-gpu")
-# options.add_argument("--disable-features=NetworkService")
-# options.add_argument("--window-size=1920x1080")
-# options.add_argument("--disable-features=VizDisplayCompositor")
-# UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-# options.add_argument('--user-agent=' + UA)
-# mobile_emulation = { "deviceName": "Galaxy S5" }
-# options.add_experimental_option("mobileEmulation", mobile_emulation)
-
-prefecture_list = ['愛知県']#,'東京都'
+prefecture_list = ['静岡県','岐阜県','愛知県','三重県']#,'東京都'
 
 line_token = os.getenv('LINE_TOKEN')
 #print(line_token)
@@ -86,43 +71,47 @@ for prefecture in prefecture_list:
         cols = ['機種名', '台番号', 'G数', '差枚', 'BB', 'RB', 'ART', 'BB確率', 'RB確率', 'ART確率','合成確率','店舗名']
         ichiran_all_tennpo_df = pd.DataFrame(index=[], columns=cols)
         yesterday = datetime.date.today() + datetime.timedelta(days=-1)
-        browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
         url = f'https://{os.getenv("SCRAPING_DOMAIN")}/%E3%83%9B%E3%83%BC%E3%83%AB%E3%83%87%E3%83%BC%E3%82%BF/{prefecture}/'
-        browser.get(url)
-        html = browser.page_source.encode('utf-8')
-        soup = BeautifulSoup(html, 'lxml')
-        tenpo_ichiran_df = pd.read_html(html)[-1]
-        #print(tenpo_ichiran_df['ホール名'])
-
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        table = soup.find_all('table')
+        tenpo_ichiran_df =pd.read_html(str(table))[-1]
+        print(tenpo_ichiran_df)
         i = 0
         for tenpo_name in tenpo_ichiran_df['ホール名'] :#tenpo_ichiran_df['ホール名']
             try:
-                #print(tenpo_name)
+                #time.sleep(1)
+                print(tenpo_name)
                 url = f'https://{os.getenv("SCRAPING_DOMAIN")}/{yesterday.strftime("%Y-%m-%d")}-{tenpo_name}'
-                browser.get(url)
-                html = browser.page_source.encode('utf-8')
-                dfs = pd.read_html(html)
+                res = requests.get(url)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                table = soup.find('table')
+                dfs =pd.read_html(str(table))
                 #display(tenpo_df)
-                time.sleep(1)
+                #time.sleep(1)
                 for df in  dfs:
-                #print(df.columns)
-                    if '機種名' in list(df.columns):
-                        ichiran_df = df
-                        ichiran_df['日付'] = yesterday.strftime('%Y-%m-%d')
-                        ichiran_df['店舗名'] = tenpo_name
-                        print(tenpo_name)
-                        ichiran_df['Nのつく日'] = yesterday.strftime('%d')[-1]
-                        ichiran_df['都道府県'] = prefecture 
-                        ichiran_df['機種名'] = ichiran_df['機種名'].map(removal_text)
-                        ichiran_all_tennpo_df =  pd.concat([ichiran_all_tennpo_df, ichiran_df])
-                        break
-            except:
-                time.sleep(1)
+                    try:
+                        if '機種名' in list(df.columns):
+                            ichiran_df = df
+                            ichiran_df['日付'] = yesterday.strftime('%Y-%m-%d')
+                            ichiran_df['店舗名'] = tenpo_name
+                            #print(tenpo_name)
+                            ichiran_df['Nのつく日'] = yesterday.strftime('%d')[-1]
+                            ichiran_df['都道府県'] = prefecture 
+                            ichiran_df['機種名'] = ichiran_df['機種名'].map(removal_text)
+                            ichiran_all_tennpo_df =  pd.concat([ichiran_all_tennpo_df, ichiran_df])
+                            break
+                        i += 1
+                    except Exception as e:
+                        print(tenpo_name,e)
+                        #time.sleep(1)
+                        continue
+            except Exception as e:
+                print(tenpo_name,e)
                 continue
-            i += 1
+
             # if i > 0:
             #     break
-        browser.quit()
         cols = ichiran_all_tennpo_df.columns.tolist()
         cols = cols[-4:] + cols[:-4]
         ichiran_all_tennpo_df = ichiran_all_tennpo_df[cols]  #    OR    df = df.ix[:, cols]
